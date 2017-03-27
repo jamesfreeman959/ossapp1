147acbba7567866de2bc162058071e66cf75e807
root <root@azure-workshop.f1s4oh0hjarell3t4fg3hewmig.zx.internal.cloudapp.net>
2017-03-27 15:44:29 +0000

    Added initial version

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>This is a VERY simple PHP application</title>
+    </head>
+
+    <body>
+        
+        <h1>How random! <?php echo rand(1,100); ?></h1>
+    </body>
+</html>
+
